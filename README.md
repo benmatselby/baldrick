13c5add43b12f26288374a6816eb0e1d84845b93
Ben Selby <benmatselby@gmail.com>
2018-02-04 20:18:41 +0000

    A very minimal slack bot
    * Currently only gives you a status list of jenkins jobs

---
+++ README.md
@@ -0,0 +1,15 @@
+Baldrick
+========
+
+A slack bot that can do stuff for the team
+
+## Configuration
+
+```
+$ export BALDRICK_SLACK_TOKEN='slack-token'
+$ export BALDRICK_JENKINS_URL='https://jenkins.url'
+```
+
+## What can we do
+
+* `jenkins list` - Displays a list of Jenkins jobs
---
+++ index.js
@@ -0,0 +1,67 @@
+const _ = require('underscore');
+const { WebClient, RtmClient, RTM_EVENTS } = require('@slack/client');
+const token = process.env.BALDRICK_SLACK_TOKEN;
+
+const rtm = new RtmClient(token, {
+  dataStore: false,
+  useRtmConnect: true,
+});
+
+const slack = new WebClient(token);
+const jenkins = require('jenkins')({ baseUrl: process.env.BALDRICK_JENKINS_URL });
+
+console.log('Jenkins: '+process.env.BALDRICK_JENKINS_URL);
+
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+    if (message.type === 'message' && /jenkins list/i.test(message.text)) {
+        var channel = message.channel;
+
+
+        jenkins.job.list(function(err, data) {
+            if (err) {
+                slack.chat.postMessage(channel, 'Whoopsie daisy... Unable to get a list of Jenkins jobs')
+                    .then((res) => {})
+                    .catch(console.error);
+                return;
+            }
+
+            var attachments = [],
+                counter = 0;
+
+            _.each(data, function(job) {
+                if (counter > 30) {
+                    return false;
+                }
+
+                switch (job.color) {
+                    case "red":
+                        color = "danger";
+                        break;
+                    case "blue":
+                        color = "good";
+                        break;
+                    default:
+                        color = "#d8d8d8";
+                        break;
+                }
+
+                attachments.push({
+                    fallback: job.name,
+                    text: "<" + job.url + "|" + job.name + ">",
+                    color: color
+                });
+                counter++;
+            });
+
+            var message = {
+                attachments: attachments
+            }
+
+            slack.chat.postMessage(channel, 'Jenkins jobs', message)
+                .then((res) => {})
+                .catch(console.error);
+        });
+    }
+});
+
+rtm.start();
