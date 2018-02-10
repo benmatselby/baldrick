8abe61279d1e9923dbfb5dc0b241c92805cdfb43
Ben Selby <ben.selby@emisgroupplc.com>
2018-02-10 10:15:41 +0000

    Refactor so we can "namespace" commands
    Don't really mean namespace, but it's about making it cleaner as to what
    we have integration and where the code will be
    
    Converted to use promises

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const _ = require('underscore')
 const token = process.env.BALDRICK_SLACK_TOKEN
 const { WebClient, RtmClient, RTM_EVENTS } = require('@slack/client')
 const rtm = new RtmClient(token, {
@@ -6,70 +5,21 @@ const rtm = new RtmClient(token, {
   useRtmConnect: true
 })
 const slack = new WebClient(token)
-const jenkins = require('jenkins')({ baseUrl: process.env.BALDRICK_JENKINS_URL })
-
-console.log('Jenkins: ' + process.env.BALDRICK_JENKINS_URL)
+const jenkins = require('./lib/jenkins')
 
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-  if (message.type === 'message' && /jenkins list/i.test(message.text)) {
-    var channel = message.channel
-    var keywords = message.text.split(' ')
-    var onlyGood = false
-    var onlyBad = false
-
-    if (!_.isEmpty(_.intersection(keywords, ['success', 'good']))) {
-      onlyGood = true
-    }
-
-    if (!_.isEmpty(_.intersection(keywords, ['failed', 'broken', 'bad']))) {
-      onlyBad = true
-    }
-
-    jenkins.job.list(function (err, data) {
-      if (err) {
-        slack.chat.postMessage(channel, 'Whoopsie daisy... Unable to get a list of Jenkins jobs')
-            .then((res) => { })
-            .catch(console.error)
-        return
-      }
-
-      var attachments = []
-
-      _.each(data, function (job) {
-        var color = '#d8d8d8'
-        switch (job.color) {
-          case 'red':
-            color = 'danger'
-            break
-          case 'blue':
-            color = 'good'
-            break
-        }
-
-        if (job.color !== 'red' && onlyBad) {
-          return
-        }
-
-        if (job.color !== 'blue' && onlyGood) {
-          return
-        }
-
-        attachments.push({
-          fallback: job.name,
-          text: '<' + job.url + '|' + job.name + '>',
-          color: color
-        })
-      })
-
-      var message = {
-        attachments: attachments.slice(0, 100), // Should be a config item
-        as_user: true
-      }
-
-      slack.chat.postMessage(channel, 'Jenkins jobs', message)
-                .then((res) => { })
-                .catch(console.error)
-    })
+  if (message.type === 'message' && /jenkins/i.test(message.text)) {
+    jenkins.handleMessage(message)
+      .then(data => (
+        slack.chat.postMessage(message.channel, data.text, data)
+          .then((res) => { })
+          .catch(console.error)
+     ))
+     .catch(() => (
+        slack.chat.postMessage(message.channel, 'Something went wrong with the space time continuum', {as_user: true})
+          .then((res) => { })
+          .catch(console.error)
+     ))
   }
 })
 
